52bd7f26345138837c23538610c68a9efe174428
Kashi Samaraweera <samaraweera.kashi@abc.net.au>
2019-02-01 14:12:01 +1100

    Added photos indexing  support

---
+++ src/layouts/PhotosIndexLayout/PhotosIndexLayout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './photos-index-layout.css'
+
+class PhotosIndexLayout extends React.Component {
+  render() {
+    const { children } = this.props;
+    return children
+  }
+}
+
+export default PhotosIndexLayout
