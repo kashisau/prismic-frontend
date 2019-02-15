d6cbb856b8c14184079db5f2d0d028bbf478eb4a
Kashi Samaraweera <samaraweera.kashi@abc.net.au>
2019-02-15 17:03:35 +1100

    Updated styles.

--- src/components/PhotoStage/PhotoStage.js
+++ src/components/PhotoStage/PhotoStage.js
@@ -2,6 +2,11 @@ import React from 'react'
 import styles from './photo-stage.module.css'
 import classNames from 'classnames/bind'
 import Img from 'gatsby-image'
+import { ago } from 'time-ago'
+import iconCamera from '../../images/icon-camera.svg'
+import iconAperture from '../../images/icon-apeture.svg'
+import iconFocalLength from '../../images/icon-lens.svg'
+import iconShutterSpeed from '../../images/icon-shutter.svg'
 
 const PhotoStage = (photoData) => {
   const { photo } = photoData
@@ -10,25 +15,21 @@ const PhotoStage = (photoData) => {
   const photoDate = new Date(exif.tags.DateTimeOriginal*1000)
   const exposureTime = exif.tags.ExposureTime
   const exposureString = (exposureTime >= 1)? `${exposureTime}s` : `1/${parseInt(1/exposureTime)}s`
-  const lens = exif.tags.LensModel
-  const classnames = classNames(styles.fullPhoto, styles[`ratio${photo.aspectRatio.split(':').join('x')}`])
 
   return (<article>
       <div className={classNames(styles.boundary)}>
         <Img
-          className={classnames}
           fluid={photo.file.localFile.childImageSharp.fluid}
           alt={photoTitle} />
       </div>
       <header>
-        <h1>{photoTitle}</h1>
+        <h1 className={styles.h1}>{photoTitle}</h1>
         <ul className={styles.data}>
-          <li className={styles.photoDate}><time dateTime={photoDate.toISOString()}>{`${photoDate.toLocaleDateString()} at ${photoDate.toLocaleTimeString()}`}</time></li>
-          <li className={styles.cameraModel}><span hidden>Camera: </span>{exif.tags.Model}</li>
-          <li className={styles.focalLength}><span hidden>Focal length: </span>{exif.tags.FocalLength}mm</li>
-          <li className={styles.apeture}><span hidden>Apeture: </span>f/{exif.tags.FNumber}mm</li>
-          <li className={styles.shutterSeed}><span hidden>Shutter speed: </span>{exposureString}</li>
-          <li className={styles.lens}><span hidden>Lens: </span>{lens}</li>
+          <li className={styles.photoDate}><time dateTime={photoDate.toISOString()}>{ago(photoDate)}</time></li>
+          <li className={styles.cameraModel}><img className={styles.infoIcon} src={iconCamera} alt="Camera" />{exif.tags.Model}</li>
+          <li className={styles.focalLength}><img className={styles.infoIcon} src={iconFocalLength} alt="Focal length" />{exif.tags.FocalLength}mm</li>
+          <li className={styles.aperture}><img className={styles.infoIcon} src={iconAperture} alt="Aperture" />f/{exif.tags.FNumber}</li>
+          <li className={styles.shutterSeed}><img className={styles.infoIcon} src={iconShutterSpeed} alt="Shutter speed" />{exposureString}</li>
         </ul>
       </header>
       <p dangerouslySetInnerHTML={{__html: photo.description.html}} />
--- src/layouts/PhotosIndexLayout/PhotosIndexLayout.js
+++ src/layouts/PhotosIndexLayout/PhotosIndexLayout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PhotoListItem from '../../components/PhotoListItem'
-import styles from './photos-index-layout.module.css'
+import styles from './photos-index-layout.css'
 
 const PhotosIndexLayout = ({photos, children}) => {
   return (
