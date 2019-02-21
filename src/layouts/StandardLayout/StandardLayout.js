fde489ac9209ddfe7e1040da9744a444475f476f
Kashi Samaraweera <kashi@kashis.com.au>
2019-02-22 07:35:40 +1100

    Menu toggle.

--- src/layouts/StandardLayout/StandardLayout.js
+++ src/layouts/StandardLayout/StandardLayout.js
@@ -1,15 +1,21 @@
-import React, { Fragment } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import SiteNav from '../../components/SiteNav'
 import classNames from 'classnames/bind'
 
 import styles from './standard-layout.module.css'
 
-const StandardLayout = ({children, menuActive}) => {
+const StandardLayout = ({children}) => {
+  const [menuActive, setMenuActive] = useState(true)
+  const activeClasses = classNames.bind(styles)
+
   return (
-    <div className={styles.page}>
+    <div className={activeClasses('page', { 'menuActive': menuActive })}>
       <header className={styles.siteHeader}>
         <h1 className={styles.logoType}>Kashi Samaraweera</h1>
+        <button
+          className={styles.menuToggle}
+          onClick={e => setMenuActive(!menuActive)}>Menu</button>
         <SiteNav className={styles.siteNav} />
         {children}
       </header>
