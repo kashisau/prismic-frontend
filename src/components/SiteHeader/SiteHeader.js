90e262aa7a930b8a9b12f99e9e0e4ad3de2e997a
Kashi Samaraweera <kashi@kashis.com.au>
2019-03-01 06:47:24 +1100

    Workingg menu for mobile.

--- src/components/SiteHeader/SiteHeader.js
+++ src/components/SiteHeader/SiteHeader.js
@@ -4,24 +4,36 @@ import classNames from 'classnames/bind'
 
 import styles from './site-header.module.css'
 
+const DEFAULT_PADDING_MIN = 4
+const DEFAULT_PADDING_MAX = 24
+
 class SiteHeader extends Component {
 
-  headings = createRef()
-  menuButton = createRef()
-  siteNav = createRef()
-
-  DEFAULT_PADDING_MIN = 4
-  DEFAULT_PADDING_MAX = 24
+  logos = createRef()
+  pageHeadings = createRef()
+  menuToggle = createRef()
 
   state = {
     menuActive: true,
     siteNameActive: false,
-    headerPadding: this.DEFAULT_PADDING_MIN,
+    headerPadding: DEFAULT_PADDING_MIN,
     lastScrollPos: 0
   }
 
   watchScroll = () =>  {
+    const { headerPadding, lastScrollPos, menuActive } = this.state
+    if (menuActive) return
 
+    const scrollPos = window.scrollY
+    const scrollDelta = scrollPos - lastScrollPos
+    const newHeaderPadding = headerPadding - scrollDelta
+
+    this.setState({ lastScrollPos: scrollPos })
+
+    if (newHeaderPadding < DEFAULT_PADDING_MIN) return
+    if (newHeaderPadding > DEFAULT_PADDING_MAX) return
+
+    this.setState({ headerPadding: newHeaderPadding })
   }
 
   componentDidMount() {
@@ -33,7 +45,7 @@ class SiteHeader extends Component {
   }
 
   render() {
-    const { menuActive, siteNameActive } = this.state
+    const { menuActive, siteNameActive, headerPadding } = this.state
     const { title, subtitle } = this.props
 
     const classes = classNames.bind(styles)
@@ -42,20 +54,26 @@ class SiteHeader extends Component {
       { 'menuActive': menuActive }
     )
 
+    const scrollMargins = {
+      marginTop: menuActive? DEFAULT_PADDING_MAX : headerPadding,
+      marginBottom: menuActive? DEFAULT_PADDING_MAX : headerPadding
+    }
+
     return (
       <header className={classNames(styles.siteHeader, activeClasses)} ref={this.props.innerRef}>
-        <div className={styles.logos}>
+        <div className={styles.logos} ref={this.logos}>
           <span className={styles.logoType}>Sliding menu</span>
           <span className={styles.logoTypeSubtitle}>Standard web app</span>
         </div>
-        <hgroup className={styles.pageHeadings}>
+        <hgroup className={styles.pageHeadings} ref={this.pageHeadings}>
           {subtitle && <h2 className={styles.subtitle}>{subtitle}</h2>}
           {title && <h1 className={styles.title}>{title}</h1>}
         </hgroup>
         <button
           className={styles.menuToggle}
           onClick={_ => this.setState({ menuActive: !menuActive})}
-          ref={ref => this.menuButton}>Menu</button>
+          ref={this.menuToggle}
+          style={scrollMargins}>Menu</button>
         <SiteNav className={styles.siteNav} isActive={menuActive} ref={this.siteNav} />
       </header>
     )
