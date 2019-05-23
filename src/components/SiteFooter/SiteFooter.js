ae4adc8f9ce6dc9562305e5783c1b342e3c0c4ce
Kashi Samaraweera <kashi@kashis.com.au>
2019-05-23 22:35:26 +0545

    Temporarily hardcoded footer copy.

--- src/components/SiteFooter/SiteFooter.js
+++ src/components/SiteFooter/SiteFooter.js
@@ -11,27 +11,27 @@ import logoCloudflare from '../../images/logo-cloudflare.svg'
 
 export default () => {
   const SiteFooterQuery = useStaticQuery(graphql`
-  query SiteFooterQuery {
-    allPrismicSiteMetadata {
-      edges {
-        node {
-          data {
-            footer_blurb {
-              html
+    query SiteFooterQuery {
+      allPrismicSiteMetadata {
+        edges {
+          node {
+            data {
+              footer_blurb {
+                html
+              }
             }
           }
         }
       }
     }
-  }
   `)
-  console.log("SiteFooterQuery: ", SiteFooterQuery)
+
   const footerBlurb = SiteFooterQuery.allPrismicSiteMetadata.edges[0].node.data.footer_blurb
   return (
     <footer className={styles.footer}>
       <div className={styles.footerContent}>
         <h1 className={styles.footerTitle}>Where I'm active</h1>
-        <p className={styles.footerContactBlurb} dangerouslySetInnerHTML={{__html: footerBlurb.html}} />
+        <p className={styles.footerContactBlurb}>Examples of my work can be found on <a  href="https://github.com/kashisau">GitHub</a> or <a  href="https://bitbucket.org/kashisau/">BitBucket</a>. You can keep up with my adventures via <a  href="https://instagram.com/kashisamaraweera">Instagram</a>, track my progress as an aspiring shutterbug on my <a  href="https://www.instagram.com/kashis.photo/">alternate Instagram account</a>; read about my journey as an Australian Volunteer on my co-authored blog <a  href="https://medium.com/the-unlikely-sherpas">The Unlikely Sherpas</a>, or the exports of my ongoing study in philosophy &amp; linguistics on my blog, <a  href="https://enroute.kashis.com.au">En Route</a>. Or you can connect to my professional network via <a  href="https://au.linkedin.com/in/kashisau">LinkedIn</a>, and otherwise get in touch via email, <a  href="mailto:Kashi%20Samaraweera%20%3Ckashi@kashis.com.au%3E">kashi@kashis.com.au</a>.</p>
         <div className={styles.viewSource}>
           <h1 className={styles.sourceTitle}>Built with the following</h1>
           <p>This is an open source project, built on the following technologies.</p>
