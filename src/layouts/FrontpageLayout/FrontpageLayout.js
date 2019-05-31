3759eeaef04fbc1607c3025507e653735721f341
Kashi Samaraweera <kashi@kashis.com.au>
2019-05-31 12:24:53 +0545

    Subtle animations added for image assets.

--- src/layouts/FrontpageLayout/FrontpageLayout.js
+++ src/layouts/FrontpageLayout/FrontpageLayout.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import StandardLayout from '../StandardLayout';
+import { Controller, Scene } from 'react-scrollmagic';
+import { Tween } from 'react-gsap'
+
 import classnames from 'classnames/bind'
 import Img from 'gatsby-image'
 
@@ -12,7 +15,6 @@ const FrontpageLayout = ({
     children
   }) => {
   const { title: heroTitle, org: heroSubtitle, blurb: heroArticleBlurb, product_image: heroProductImage } = workHero
-  
   return (
     <StandardLayout>
       <div className={styles.hero}>
@@ -21,10 +23,29 @@ const FrontpageLayout = ({
         </div>
         <h1 className={styles.pageTitle}>Latest<span className={styles.fullTitle}>&nbsp;work</span></h1>
         <article className={styles.heroArticle}>
-          <Img
-            className={styles.heroImage}
-            fluid={heroProductImage.localFile.childImageSharp.fluid}
-            alt={heroTitle.text} />
+          <Controller>
+            <Scene duration={400}>
+              {progress =>
+                <Tween
+                  to={{
+                    x: '0%',
+                    scale: '1'
+                  }}
+                  from={{
+                    x: '1%',
+                    scale: '1.05'
+                  }}
+                  totalProgress={progress}
+                  paused>
+                    <div className={styles.heroImage}>
+                      <Img
+                        fluid={heroProductImage.localFile.childImageSharp.fluid}
+                        alt={heroTitle.text} />
+                    </div>
+                </Tween>
+              }
+            </Scene>
+          </Controller>
           <hgroup className={styles.articleHeadings}>
             <h2 className={classnames(styles.heroTitle)}>{heroTitle.text}</h2>
             <h3 className={classnames(styles.heroSubtitle)}>{heroSubtitle.text}</h3>
@@ -34,18 +55,41 @@ const FrontpageLayout = ({
       </div>
       {featuredWorks && <section className={styles.latestWorkSpill}>
         <h1 className={styles.bodyTitle}>Previous<span className={styles.fullTitle}>&nbsp;work</span></h1>
-        {featuredWorks.map((work, index) => 
-          <article className={styles.workArticle} key={index}>
-            <Img
-              className={styles.workImage}
-              fluid={work.product_image.localFile.childImageSharp.fluid}
-              alt={work.title.text} />
-            <hgroup className={styles.workHeadings}>
-              <h2 className={styles.workTitle}>{work.title.text}</h2>
-              <h3 className={styles.workSubtitle}>{work.org.text}</h3>
-            </hgroup>
-            <div className={styles.workBlurb} dangerouslySetInnerHTML={{ __html: work.blurb.html }} />
-          </article>)}
+        {featuredWorks.map(
+          (work, index) => {
+            return <article className={styles.workArticle} key={index} id={`work-item-${index}`}>
+              <Controller>
+                <Scene duration={600}
+                  triggerElement={`#work-item-${index}`}
+                  triggerHook="onEnter">
+                  {progress =>
+                    <Tween
+                      to={{
+                        x: `0%`,
+                        scale: '1'
+                      }}
+                      from={{
+                        x: `${index % 2 === 0 ? -3 : 3}%`,
+                        scale: '1.1'
+                      }}
+                      totalProgress={progress}
+                      paused>
+                        <div className={styles.workImage}>
+                        <Img
+                          fluid={work.product_image.localFile.childImageSharp.fluid}
+                          alt={work.title.text} />
+                        </div>
+                    </Tween>
+                  }
+                </Scene>
+              </Controller>
+              <hgroup className={styles.workHeadings}>
+                <h2 className={styles.workTitle}>{work.title.text}</h2>
+                <h3 className={styles.workSubtitle}>{work.org.text}</h3>
+              </hgroup>
+              <div className={styles.workBlurb} dangerouslySetInnerHTML={{ __html: work.blurb.html }} />
+            </article>
+          })}
         </section>}
     </StandardLayout>
   )
